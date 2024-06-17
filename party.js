4daddc0a1a20839f65e1b41b194197eb1d9eb907
sonikawasi <168327392+sonikawasi@users.noreply.github.com>
2024-06-17 10:18:03 +0530

    Add files via upload

---
+++ party.js
@@ -0,0 +1,4 @@
+document.getElementById('playButton').addEventListener('click', function() {
+    var url = 'https://partyrock.aws/u/Sombari123/GWsUe7U1-/GrammarMaster%3A-A-Comprehensive-Grammar-Learning-and-Practice-App'; // Replace with the URL you want to open
+    window.open(url, '_blank');
+});
